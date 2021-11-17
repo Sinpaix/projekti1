bf242cfaa958d77291fd41eb63b9bcc9aaa1d112
Ville Paasonen <Ville.Paasonen@edu.savonia.fi>
2021-11-17 12:08:46 +0200

    käyttöliittymän muutoksia työntekijähallinta tabia aloitettu

--- Projekti1/Form1.cs
+++ Projekti1/Form1.cs
@@ -16,5 +16,15 @@ namespace Projekti1
         {
             InitializeComponent();
         }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox4_TextChanged(object sender, EventArgs e)
+        {
+
+        }
     }
 }
