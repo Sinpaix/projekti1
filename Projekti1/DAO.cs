a887b999be23d102ad5cc66d3771bbcabaaec49d
Simo Lyytinen <Simo.Lyytinen@edu.savonia.fi>
2021-11-18 19:59:41 +0200

    Lisätty controller ja DAO luokat

---
+++ Projekti1/DAO.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Projekti1.Models;
+using MySql.Data.MySqlClient;
+
+namespace Projekti1
+{
+    class DAO
+    {
+        static MySqlConnection conn = null;
+        static string myConnectionString = "server=127.0.0.1;uid=root;" +
+             "pwd=root;database=sairaalakanta";
+
+    }
+}
--- Projekti1/MainForm.Designer.cs
+++ Projekti1/MainForm.Designer.cs
@@ -107,7 +107,7 @@ namespace Projekti1
             this.tabControl1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.tabControl1.Name = "tabControl1";
             this.tabControl1.SelectedIndex = 0;
-            this.tabControl1.Size = new System.Drawing.Size(1605, 1364);
+            this.tabControl1.Size = new System.Drawing.Size(1875, 1364);
             this.tabControl1.TabIndex = 0;
             // 
             // tabPage1
@@ -274,7 +274,7 @@ namespace Projekti1
             this.tabPage3.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.tabPage3.Name = "tabPage3";
             this.tabPage3.Padding = new System.Windows.Forms.Padding(4, 5, 4, 5);
-            this.tabPage3.Size = new System.Drawing.Size(1597, 1331);
+            this.tabPage3.Size = new System.Drawing.Size(1867, 1331);
             this.tabPage3.TabIndex = 1;
             this.tabPage3.Text = "Vuorojen syöttö";
             this.tabPage3.UseVisualStyleBackColor = true;
@@ -636,7 +636,7 @@ namespace Projekti1
             this.tabPage2.Location = new System.Drawing.Point(4, 29);
             this.tabPage2.Name = "tabPage2";
             this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage2.Size = new System.Drawing.Size(1597, 1331);
+            this.tabPage2.Size = new System.Drawing.Size(1867, 1331);
             this.tabPage2.TabIndex = 4;
             this.tabPage2.Text = "Työntekijöiden kiinnitys";
             this.tabPage2.UseVisualStyleBackColor = true;
@@ -645,7 +645,7 @@ namespace Projekti1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1605, 908);
+            this.ClientSize = new System.Drawing.Size(1875, 1044);
             this.Controls.Add(this.tabControl1);
             this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.Name = "MainForm";
